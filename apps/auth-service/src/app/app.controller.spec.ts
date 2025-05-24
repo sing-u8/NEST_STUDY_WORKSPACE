276fd19279e24b613de6b48506fe60d4bc1a92ff
ParkSingyu <singyu13@gmail.com>
2025-05-25 01:42:13 +0900

    - init: 프로젝트 초기 세팅
    - description
    
    -- Nx로 Nestjs 백엔드 모노레포 세팅
    -- biome 세팅 완료

--- apps/auth-service/src/app/app.controller.spec.ts
+++ apps/auth-service/src/app/app.controller.spec.ts
@@ -1,21 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test, type TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
-describe('AppController', () => {
-  let app: TestingModule;
+describe("AppController", () => {
+	let app: TestingModule;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-  });
+	beforeAll(async () => {
+		app = await Test.createTestingModule({
+			controllers: [AppController],
+			providers: [AppService],
+		}).compile();
+	});
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
-    });
-  });
+	describe("getData", () => {
+		it('should return "Hello API"', () => {
+			const appController = app.get<AppController>(AppController);
+			expect(appController.getData()).toEqual({ message: "Hello API" });
+		});
+	});
 });
--- apps/auth-service/webpack.config.js
+++ apps/auth-service/webpack.config.js
@@ -1,20 +1,20 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
+const { join } = require("node:path");
 
 module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/auth-service'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
-    }),
-  ],
+	output: {
+		path: join(__dirname, "../../dist/apps/auth-service"),
+	},
+	plugins: [
+		new NxAppWebpackPlugin({
+			target: "node",
+			compiler: "tsc",
+			main: "./src/main.ts",
+			tsConfig: "./tsconfig.app.json",
+			assets: ["./src/assets"],
+			optimization: false,
+			outputHashing: "none",
+			generatePackageJson: true,
+		}),
+	],
 };
